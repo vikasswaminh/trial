2dc78229caa28e4fb100ff3d55f3d4be30c80a39
vikasswaminh <67453408+vikasswaminh@users.noreply.github.com>
2020-11-16 11:57:46 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+export default () => (
+  <div className="p-4 shadow rounded bg-white">
+    <h1 className="text-purple leading-normal">Next.js</h1>
+    <p className="text-grey-dark">with Tailwind CSS</p>
+  </div>
+);
